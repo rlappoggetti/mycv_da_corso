f0991f16275154c280d30b406c5cbb44e3f804f3
Roberto Appoggetti <roberto.appoggetti@spindox.it>
2025-08-29 11:33:10 +0200

    aggiunta per svil 2

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -82,7 +82,7 @@ export class UsersController {
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
-      throw new NotFoundException('user indeed not found really');
+      throw new NotFoundException('user indeed not found really fatto per svil2');
     }
     return user;
   }
